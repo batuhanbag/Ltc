a176cbdaee05412012031c4ab8f5f28ba2380eba
Batuhan Bag <batuhan.bag@actimi.com>
2024-07-29 22:35:30 +0300

    fix: reverted button #0

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,8 +5,49 @@ import {
   type GestureResponderEvent,
   Text,
 } from 'react-native';
+import { getTheme } from '../utils/init';
+import { getFontSize, scale, verticalScale } from '../utils/window';
+
+const styles = {
+  baseStyle: {
+    borderRadius: 60,
+    paddingHorizontal: scale(10),
+    paddingVertical: verticalScale(9),
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  textStyle: {
+    fontSize: getFontSize(15),
+  },
+  withIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: scale(10),
+  },
+  large: {
+    width: '100%',
+  },
+  medium: {
+    width: '50%',
+  },
+  pill: {
+    width: '40%',
+  },
+};
 
 interface ButtonProps {
+  type?: 'primary' | 'secondary' | 'tertiary' | 'pill';
+  withIcon?: React.ReactNode;
+  size?: 'medium' | 'large';
+  loading?: boolean;
+  disabled?: boolean;
+  onPress?: (event: GestureResponderEvent) => void;
+  text: string;
+  styles?: any;
+}
+
+interface DefualtButtonProps {
   buttonStyle?: any;
   textStyle?: any;
   backgroundColor: string;
@@ -23,19 +64,148 @@ interface ButtonProps {
   externalStyle?: any;
 }
 
-const Button: React.FC<ButtonProps> = ({ ...props }) => {
+const { colors } = getTheme();
+
+const buttonOptions = (type: string | undefined) => {
+  switch (type) {
+    case 'secondary':
+      return {
+        component: SecondaryButton,
+        backgroundColor: colors.secondary,
+        color: colors.primary,
+      };
+    case 'tertiary':
+      return {
+        component: TertiaryButton,
+        backgroundColor: 'transparent',
+        color: colors.primary,
+      };
+    case 'primary':
+      return {
+        component: PrimaryButton,
+        backgroundColor: colors.primary,
+        color: colors.white,
+      };
+    case 'pill':
+      return {
+        component: PillButton,
+        backgroundColor: 'transparent',
+        color: colors.primary,
+      };
+    default:
+      return {
+        component: PrimaryButton,
+        backgroundColor: colors.primary,
+        color: colors.white,
+      };
+  }
+};
+
+const Button: React.FC<ButtonProps> = ({
+  type = 'primary',
+  withIcon,
+  size = 'large',
+  loading = false,
+  disabled = false,
+  onPress,
+  text,
+  styles,
+}) => {
+  const {
+    component: ButtonComponent,
+    backgroundColor,
+    color,
+  } = buttonOptions(type);
+
   return (
-    <TouchableOpacity disabled={props?.disabled} onPress={props?.onPress}>
-      {props?.loading ? (
-        <ActivityIndicator size="small" color={props?.activityIndicatorColor} />
+    <ButtonComponent
+      backgroundColor={backgroundColor as string}
+      color={color}
+      withIcon={withIcon}
+      size={size}
+      disabled={disabled}
+      loading={loading}
+      onPress={onPress}
+      text={text}
+      type={type}
+      externalStyle={styles}
+      activityIndicatorColor={
+        type === 'primary' ? colors.white : colors.primary
+      }
+    />
+  );
+};
+
+const ButtonComponent: React.FC<DefualtButtonProps> = ({
+  backgroundColor,
+  color,
+  withIcon,
+  size,
+  disabled,
+  loading,
+  text,
+  textColor,
+  activityIndicatorColor,
+  onPress,
+  type,
+  externalStyle,
+}) => {
+  const buttonStyle = () => [
+    styles.baseStyle,
+    withIcon ? styles.withIcon : {},
+    { backgroundColor },
+    size === 'large'
+      ? styles.large
+      : size === 'medium'
+        ? styles.medium
+        : styles.pill,
+    disabled && { backgroundColor: colors.gray },
+    (type === 'tertiary' || type === 'pill') && {
+      borderWidth: 1,
+      borderColor: disabled ? colors.gray : colors.primary,
+      backgroundColor: 'transparent',
+    },
+  ];
+  const textStyle = () => [
+    styles.textStyle,
+
+    {
+      color: textColor || color,
+      fontSize: type === 'pill' ? getFontSize(14) : getFontSize(16),
+    },
+    disabled && { color: colors.gray },
+  ];
+
+  return (
+    <TouchableOpacity
+      style={[buttonStyle(), externalStyle ? { ...externalStyle } : {}]}
+      disabled={disabled}
+      onPress={onPress}
+    >
+      {loading ? (
+        <ActivityIndicator size="small" color={activityIndicatorColor} />
       ) : (
         <>
-          <Text>{props?.text}</Text>
-          {props?.withIcon && props?.withIcon}
+          <Text style={textStyle()}>{text}</Text>
+          {withIcon && withIcon}
         </>
       )}
     </TouchableOpacity>
   );
 };
+
+const PrimaryButton = (props: DefualtButtonProps) => (
+  <ButtonComponent {...props} />
+);
+const SecondaryButton = (props: DefualtButtonProps) => (
+  <ButtonComponent {...props} />
+);
+const TertiaryButton = (props: DefualtButtonProps) => (
+  <ButtonComponent {...props} />
+);
+
+const PillButton = (props: DefualtButtonProps) => (
+  <ButtonComponent {...props} />
+);
 
 export { Button };
